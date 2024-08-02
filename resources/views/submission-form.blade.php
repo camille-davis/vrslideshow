3aba79d887c7a2596b6c7aaf6803203e152d3929
Camille Davis <camilledavisdev@gmail.com>
2024-08-02 14:23:00 -0700

    Add landing page with HTML boilerplate.

---
+++ resources/views/submission-form.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel=stylesheet href="/css/style.css">
+
+        <title></title>
+    </head>
+    <body>
+        <main>
+            <div class="container">
+                <div class="inner">
+                    <div class="content">
+                        <h1 class="title page-title">
+                        </h1>
+                    </div><!-- .content -->
+                </div><!-- .inner -->
+            </div><!-- .container -->
+
+            <div class="sections">
+                <div class="container">
+                    <div class="inner">
+                    </div><!-- .inner -->
+                </div><!-- .container -->
+            </div><!-- .sections -->
+
+            <div class="prompts" id="prompts">
+            </div><!-- .prompts -->
+        </main>
+    </body>
+</html>
